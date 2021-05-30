8b3a25d72656491e1ee2b1bbad437be06f0f3781
BwanaQ <thunjawax@gmail.com>
2021-05-30 15:13:59 +0300

    add procfile and edit package.json

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const path = require("path");
+
 const app = express();
-app.use(express.static(__dirname + "dist/winery"));
-app.get("/*", (req, res, next) => {
-  res.sendFile(path.join(__dirname + "dist/dictionary/index.html"));
+
+app.use(express.static("./dist/winery"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/winery" });
 });
 
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`);
