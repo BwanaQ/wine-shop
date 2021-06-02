0253379f46f7d6113413c6b9e086f808fa2397cd
BwanaQ <thunjawax@gmail.com>
2021-06-02 11:51:21 +0300

    ft - add to cart functionality

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
+import { Wine } from './../../models/wine';
+import { MessengerService } from './../../services/messenger.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
   cartItems = [];
-  constructor() {}
+  bottleTotal = 0;
+  caseTotal = 0;
+  cartTotal = 0;
+  constructor(private msg: MessengerService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.msg.getMsg().subscribe((wine: Wine) => {
+      console.log(wine);
+      this.cartItems.push({
+        productName: wine.name,
+        bottleQty: wine.bottleQty,
+        caseQty: wine.caseQty,
+        bottlePrice: wine.cost.bottle,
+        casePrice: wine.cost.case,
+      });
+      this.bottleTotal = 0;
+      this.caseTotal = 0;
+
+      this.cartItems.forEach((item) => {
+        if (item.bottleQty !== '') {
+          this.bottleTotal = item.bottleQty * item.bottlePrice;
+        }
+        if (item.caseQty !== '') {
+          this.caseTotal = item.caseQty * item.casePrice;
+        }
+        this.cartTotal += this.bottleTotal + this.caseTotal;
+      });
+    });
+  }
 }
